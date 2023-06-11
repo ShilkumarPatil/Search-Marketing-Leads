8bf7b13fb62c7744e1fdac07f2745fbbc2ace017
ShilkumarPatil <shilkumarpatil26@gmail.com>
2023-06-11 23:57:51 +0530

    searchmarketinglead1

---
+++ src/main/java/com/search/controller/SearchLeadController.java
@@ -0,0 +1,30 @@
+package com.search.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.search.dto.LeadDto;
+
+@Controller
+public class SearchLeadController {
+	
+	@Autowired
+	private LeadRestClient restClient;
+	
+	//http://localhost:8080/search
+	@RequestMapping("/search")
+	public String viewSearchLeadForm() {
+		return "search_lead";
+	}
+	
+	@RequestMapping("/findLeadById")
+	public String findLeadById(@RequestParam("id") long id, Model model) {
+		LeadDto leadDto = restClient.getLeadById(id);
+		model.addAttribute("leadDto", leadDto);
+			return "lead_info";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsps/lead_info.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Lead Information</title>
+</head>
+<body>
+	<h2>Lead Details</h2>
+	Id : ${leadDto.id}<br/>
+	First Name : ${leadDto.firstName}<br/>
+	Last Name : ${leadDto.lastName}<br/>
+	email : ${leadDto.email}<br/>
+	mobile : ${leadDto.mobile}<br/>
+
+</body>
+</html>
